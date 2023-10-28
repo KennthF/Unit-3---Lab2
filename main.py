1bc38ca50bbd9840bbe7d7e811206be9dd00584b
Kenneth <kenneth.factor@edu.sait.ca>
2023-10-28 12:45:45 -0600

    Creates the initial code

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 # The source of CSV is https://www.datablist.com/learn/csv/download-sample-csv-files
 
-with open('.\\data_people.csv', 'r') as csv_content:
-    data = csv_content.read()
+import csv
+
+with open("data_people.csv", "r") as csv_content:
+    data = csv.reader(csv_content)
+
+print("Use the 'Search' to find data of a person either by First or Last Name, Gender, and Job.")
+user = input("\nSearch:")
+
+
+
+
 
