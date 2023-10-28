54d0c4bb3b972df1cd2aa4ce923aabe168fe6b42
Kenneth <kenneth.factor@edu.sait.ca>
2023-10-28 13:03:11 -0600

    Create Gender Search (Needs fixing)

--- main.py
+++ main.py
@@ -2,13 +2,25 @@
 
 import csv
 
-with open("data_people.csv", "r") as csv_content:
-    data = csv.reader(csv_content)
+#Search the csv by gender
+def gender_search(gender):
+    result = []
+    with open("data_people.csv", "r") as csv_content:
+        data = csv.DictReader(csv_content)
+        for row in data:
+            if gender == "female" and row["Sex"] == "female":
+                result.append(row["Index"], row["First Name"], row["Last Name"])
+            elif gender == "male" and row["Sex"] == "male":
+                result = row["Index"], row["First Name"], row["Last Name"]
+    return result
+
 
 print("Use the 'Search' to find data of a person either by First or Last Name, Gender, and Job.")
-user = input("\nSearch:")
+user = input("\nSearch:").lower()
 
+content = gender_search(user)
 
+print(content)
 
 
 
