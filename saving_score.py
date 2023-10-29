75fcf0c6680b115ef9c27b5127a4b9ff9ff35588
Kenneth <kenneth.factor@edu.sait.ca>
2023-10-29 13:57:21 -0600

    Make the code to read the file

--- saving_score.py
+++ saving_score.py
@@ -9,12 +9,17 @@ def saving_score(player_num):
 
         count += 1
     
-    with open("golf.txt", "w") as golf_file:
-        golf_content = golf_file.write(str(player_information))
-    
-    return golf_content
+    with open("golf.txt", "w") as golf_write:
+        golf_write.write(str(player_information))
+
+    with open("golf.txt", "r") as golf_read:
+        content_golf = golf_read.read()
+        
+    return content_golf
 
 player_count = input("Number of Player:")
 
 content = saving_score(int(player_count))
+
+print(content)
 
