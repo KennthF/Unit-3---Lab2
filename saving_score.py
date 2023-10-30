805ab2323fea8b23bce8b5cf38065007c2339507
Kenneth <kenneth.factor@edu.sait.ca>
2023-10-30 01:45:16 -0600

    Added a validation method and looping

--- saving_score.py
+++ saving_score.py
@@ -1,13 +1,42 @@
+#Check an input if it is either a number o letter
+def validation(text):
+    if text.isnumeric():
+        return "number"
+    
+    elif text.isalpha():
+        return "letter"
+
+
 #Create a File and write the name and score in golf
 def saving_score(player_num):
     player_information = {}
-    count = 1
-    for i in range(player_num):
-        player_name = input(f"{count}.Player Name:").strip()
-        player_score = int(input(f"{player_name}'s Score:"))
-        player_information.update({player_name: player_score})
+    player_count = 1
+    counter = 0
+    check_letter = ""
+    check_number = ""
 
-        count += 1
+    #Loop the user till the player info is fill up
+    while counter != player_num:
+        player_name = input(f"\n{player_count}.Player Name:").strip()
+        check_letter = validation(player_name)
+
+        if check_letter != "letter":
+            print("\n\tInvalid Player Name\n")
+            check_letter = ""
+
+        elif check_letter == "letter":
+            player_score = input(f"{player_name}'s Score:")
+            check_number = validation(player_score)
+
+            if check_number == "number":
+                player_information.update({player_name: player_score})
+                player_count += 1
+                counter += 1
+
+            else:
+                print("\n\tInvalid Score")
+                check_number = ""
+
     
     with open("golf.txt", "w") as golf_write:
         golf_write.write(str(player_information))
@@ -17,7 +46,15 @@ def saving_score(player_num):
         
     return content_golf
 
-player_count = input("Number of Player:")
+
+check = ""
+
+while check != "number":
+    player_count = input("Number of Player:")
+    check = validation(player_count)
+
+    if check != "number":
+        print("\n\tInvalid Number\n")
 
 content = saving_score(int(player_count))
 
